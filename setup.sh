82f713bd2641b7bcfaa4d05f585b75de4c3a227f
deadlyhifi <tom.debruin@labelmedia.co.uk>
2015-02-18 08:37:56 +0000

    Clarify messages during setup.

--- setup.sh
+++ setup.sh
@@ -1,38 +1,39 @@
 #/bin/bash
 
 # Get Bitters
-echo "// Bitters ///////////////////////////////////////////////////////////////"
+echo "// Bitters //////////////////////////////////"
 echo "// Cloning Bitters…"
 git clone "git@github.com:thoughtbot/bitters.git"
-echo "// Move Bitters stylesheets…"
+echo "// Moving Bitters stylesheets…"
 mv bitters/app/assets/stylesheets/* inc/scss/framework/
-echo "// Delete unused Bitters stylesheets…"
+echo "// Deleting unused Bitters stylesheets…"
 rm inc/scss/framework/_base.scss
 rm inc/scss/framework/_grid-settings.scss
-echo "// Move overrides files…"
+echo "// Moving overrides files…"
 mv inc/scss/overrides/_grid-settings.scss inc/scss/framework/_grid-settings.scss
-echo "// Delete Bitters clone…"
+echo "// Deleting Bitters clone…"
 rm -rf bitters
-echo "// Delete overrides folder…"
+echo "// Deleting overrides folder…"
 rm -rf inc/scss/overrides
 
 # Get Normalize
-echo "// Normalize /////////////////////////////////////////////////////////////"
+echo "// Normalize ////////////////////////////////"
 echo "// Cloning Normalize…"
 git clone "git@github.com:necolas/normalize.css.git"
-echo "// Move and rename normalize.css…"
+echo "// Moving and renaming normalize.css…"
 mv normalize.css/normalize.css inc/scss/framework/_normalize.scss
-echo "// Delete Normalize clone…"
+echo "// Deleting Normalize clone…"
 rm -rf normalize.css
 
 # git and setup cleanup
-echo "// Delete .git folder…"
+echo "// Deleting .git folder…"
 rm -rf .git
-echo "// Delete .gitignore out of framework folder…"
-rm .gitignore
-echo "// Delete this setup file…"
+echo "// Deleting .gitkeep out of framework folder…"
+rm .gitkeep
+echo "// Deleting this setup file…"
 rm setup.sh
 
-echo "//////////////////////////////////////////////////////////////////////////"
-echo "// Go forth and build something amazing (mobile first!)"
-echo "//////////////////////////////////////////////////////////////////////////"
+echo "/////////////////////////////////////////////"
+echo "// ★ Go forth and build something amazing… //"
+echo "//        ✩ Remember, mobile first!        //"
+echo "/////////////////////////////////////////////"

374316b34afc04128990305e30828630b96c411f
Mark Roberts <mark.roberts@wearemarvellous.com>
2017-02-09 10:20:13 +0000

    I am dumb

--- gulp/tasks/images/build.js
+++ gulp/tasks/images/build.js
@@ -5,7 +5,7 @@ module.exports = function(gulp, config, plugins) {
 
     return function() {
         return gulp.src(config.src.images)
-            .pipe(config.production ? plugins.imagemin(config.options.imagemin) : plugins.util.noop());
+            .pipe(config.production ? plugins.imagemin(config.options.imagemin) : plugins.util.noop())
             .pipe(gulp.dest(path.resolve(config.dist.base, config.dist.images)));
     };
 };

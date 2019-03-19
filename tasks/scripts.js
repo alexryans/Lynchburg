a47c4bfb95b765af3cf9b8d702e8709b50aba02b
Mark Roberts <mark@wearemarvellous.com>
2019-03-19 16:07:39 +0100

    Babel

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -10,6 +10,20 @@ function defaultWebpackConfig(config) {
         output: {
             path: path.resolve(config.dist.dir, config.dist.scripts),
             filename: '[name].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
         }
     }
 
